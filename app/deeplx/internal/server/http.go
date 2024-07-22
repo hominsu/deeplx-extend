db8704130b90c7aa06705aaca0f5529f252f6f68
Homing So <homingso@foxmail.com>
2024-07-22 15:28:26 +0800

    feat: add server
    Signed-off-by: Homing So <homingso@foxmail.com>

---
+++ app/deeplx/internal/server/http.go
@@ -0,0 +1,54 @@
+package server
+
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/gorilla/handlers"
+
+	v1 "github.com/oio-network/deeplx-extend/api/deeplx/v1"
+	"github.com/oio-network/deeplx-extend/app/deeplx/internal/conf"
+	"github.com/oio-network/deeplx-extend/app/deeplx/internal/service"
+	"github.com/oio-network/deeplx-extend/app/deeplx/pkgs/middleware"
+)
+
+func NewHTTPServer(
+	c *conf.Server,
+	sc *conf.Secret,
+	ds *service.DeepLXService,
+	logger log.Logger,
+) *http.Server {
+	// server options
+	opts := []http.ServerOption{
+		http.Middleware(
+			recovery.Recovery(),
+			logging.Server(logger),
+			validate.Validator(),
+			middleware.Auth(sc.GetAuth().GetToken()),
+		),
+		http.Filter(
+			handlers.CORS(
+				handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+				handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+				handlers.AllowedOrigins([]string{"*"}),
+			),
+		),
+	}
+
+	if c.Http.Network != "" {
+		opts = append(opts, http.Network(c.Http.Network))
+	}
+	if c.Http.Addr != "" {
+		opts = append(opts, http.Address(c.Http.Addr))
+	}
+	if c.Http.Timeout != nil {
+		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
+	}
+	srv := http.NewServer(opts...)
+
+	v1.RegisterDeepLXServiceHTTPServer(srv, ds)
+
+	return srv
+}
